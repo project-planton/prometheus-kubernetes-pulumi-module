ec0f1768cffe1234168adc0ac7df82d63c1f8881
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:53:15 +0530

    moved to project-planton

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	prometheuskubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/kubernetes/prometheuskubernetes/v1"
+	prometheuskubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/prometheuskubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/prometheus-kubernetes-pulumi-module/pkg"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
+	"github.com/project-planton/prometheus-kubernetes-pulumi-module/pkg"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
