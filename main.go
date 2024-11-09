7490bfedf4231acf80a3d9e74621a0f3862c2e9f
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:58:12 +0530

    switched over to using local generated stubs for project-planton apis

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	prometheuskubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/prometheuskubernetes/v1"
 	"github.com/pkg/errors"
+	prometheuskubernetesv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/kubernetes/prometheuskubernetes/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/stackinput"
 	"github.com/project-planton/prometheus-kubernetes-pulumi-module/pkg"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
--- pkg/main.go
+++ pkg/main.go
@@ -1,9 +1,9 @@
 package pkg
 
 import (
-	prometheuskubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/prometheuskubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
+	prometheuskubernetesv1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/kubernetes/prometheuskubernetes/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/provider/kubernetes/pulumikubernetesprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
